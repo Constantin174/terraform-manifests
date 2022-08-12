d7f033ae2a23097af21afa8c3335f34d6337137a
Constantine Nasiescu <koka90@list.ru>
2022-08-13 00:32:59 +0500

    [DELETE] unused variables

--- manifest/variable.tf
+++ manifest/variable.tf
@@ -1,22 +1,17 @@
 variable "yandex_cloud_id" {
-  description = "YC cloud ID"
-  type = string
+  description  = "YC cloud ID"
+  type         = string
+  default      = ""
 }
 
 variable "yandex_folder_id" {
-  description = "YC folder ID"
-  type = string
+  description  = "YC folder ID"
+  type         = string
+  default      = ""
 }
 
 variable "timezone" {
-  default = "ru-central1-a"
+  type         = string
+  default      = "ru-central1-a"
 }
 
-variable "access_key" {
-  description = "Service account access key for S3 storage"
-  type = string
-}
-
-variable "secret_key" {
-  description = "Service account secret key for S3 storage"
-}
